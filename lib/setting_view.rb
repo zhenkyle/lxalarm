c105803d46069041db066178a94604e56eb20edb
Zhen Kyle <zhenkyle@gmail.com>
2012-03-25 20:48:04 +0800

    step list view & step views in switch works

--- lib/setting_view.rb
+++ lib/setting_view.rb
@@ -1,22 +1,24 @@
-class SettingStepListView < FXList
-  def initialize(p, opts)
-    super(p, :opts => opts)
-  end
-end
-
 class SettingStepView < FXVerticalFrame
-  def initialize(p, opts)
+  attr_reader :step
+  def initialize(p, step, step_list_view, opts)
     super(p, :opts => opts)
+    @step = step
+    @step_list_view = step_list_view
+    
     p = FXMatrix.new(self, 2, :opts => MATRIX_BY_COLUMNS|LAYOUT_FILL)
     FXLabel.new(p, "Name: ")
 
-    @name_target = FXDataTarget.new("Sophia")
-    name_text = FXTextField.new(p, 25,
-      :target => @name_target, :selector => FXDataTarget::ID_VALUE)
+    name_text = FXTextField.new(p, 25)
+    name_text.text = @step.name
+    name_text.connect(SEL_COMMAND) do |sender,selector,data|
+      @step.name = sender.text
+      @step_list_view.setItem(@step_list_view.currentItem,sender.text)
+    end
+    
 
     FXLabel.new(p, "Duration: ")
     duration_text = FXTextField.new(p, 25)
-
+    duration_text.text = @step.duration.to_s
     duration_text.connect(SEL_VERIFY) do |sender, sel, tentative|
       if tentative =~ /^[0-9]*$/
         false
@@ -24,7 +26,9 @@ class SettingStepView < FXVerticalFrame
         true
       end
     end
-
+    duration_text.connect(SEL_COMMAND) do |sender,selector,data|
+      @step.duration = sender.text
+    end
   end
 end
 
@@ -39,32 +43,38 @@ class SettingView < FXPacker
     alarm_name_area = FXHorizontalFrame.new(self,
       :opts => LAYOUT_FILL_X|LAYOUT_SIDE_TOP)
     
-    FXLabel.new(alarm_name_area, "Alarm: ")
+    FXLabel.new(alarm_name_area, "Alarm's name: ")
 
     alarm_name_text = FXTextField.new(alarm_name_area, 25)
+
+  	# Because @alarm.name won't be updated elsewhere, don't need SEL_UPDATE here
+    alarm_name_text.text = @alarm.name
+
     alarm_name_text.connect(SEL_COMMAND) do |sender, selector, data|
       @alarm.name = sender.text
 	end
 
-	# Because @alarm.name won't be updated elsewhere, this is not needed
-    # alarm_name_text.connect(SEL_UPDATE) do |sender, selector, data|
-    #   sender.text = @alarm.name
-	# end
+    FXHorizontalSeparator.new(self, LAYOUT_FILL_X|SEPARATOR_GROOVE)
 	
     FXLabel.new(self,"Steps:")
     
     splitter = FXSplitter.new(self,
       :opts => SPLITTER_HORIZONTAL|LAYOUT_FILL)
     
-    @setting_step_list_view = SettingStepListView.new(splitter,LAYOUT_FILL)
+    @setting_step_list_view = FXList.new(splitter, :opts => LAYOUT_FILL, :width => 100)
     
     @switcher = FXSwitcher.new(splitter, :opts => LAYOUT_FILL)
-#    @switcher.connect(SEL_UPDATE) do
-#      @switcher.current = @setting_step_list_view.currentItem
-#    end
     
-    @setting_step_list_view.appendItem("aaa")
-    SettingStepView.new(@switcher,LAYOUT_FILL)
+    @setting_step_list_view.connect(SEL_COMMAND) do |sender,selector,data|
+      @switcher.current = sender.currentItem
+    end
+    
+    
+    # Do with steps
+    alarm.each do |step|
+       @setting_step_list_view.appendItem(step.name)
+       step_view = SettingStepView.new(@switcher, step, @setting_step_list_view, LAYOUT_FILL)
+    end
     
     add_terminating_buttons
   end
